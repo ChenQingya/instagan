314d5f1892082037ffa683d44936fd284bf365f9
chenqingya <284591895@qq.com>
2019-11-19 13:58:24 +0800

    instagan --suffix 2019-11-18-01 没用upsamplelayer，改n_blocks为6

--- models/networks.py
+++ models/networks.py
@@ -286,10 +286,10 @@ class ResnetSetGenerator(nn.Module):
         model = []
         for i in range(n_downsampling):
             mult = 2**(n_downsampling - i)
-            # model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2), kernel_size=3, stride=2, padding=1, output_padding=1, bias=use_bias),
-            #          norm_layer(int(ngf * mult / 2)),
-            #          nn.ReLU(True)]
-            model += [UpsampleConvLayer(ngf * mult, int(ngf * mult / 2), kernel_size=3, stride=1, upsample=2)]
+            model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2), kernel_size=3, stride=2, padding=1, output_padding=1, bias=use_bias),
+                      norm_layer(int(ngf * mult / 2)),
+                      nn.ReLU(True)]
+            # model += [UpsampleConvLayer(ngf * mult, int(ngf * mult / 2), kernel_size=3, stride=1, upsample=2)]
         model += [nn.ReflectionPad2d(3)]
         model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
         model += [nn.Tanh()]
